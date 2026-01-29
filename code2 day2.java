517cafa1f3e90ab5671d5dc7bdf7c415914590a4
likhithaharish04 <likhithaharish134@gmail.com>
2026-01-29 15:34:59 +0530

    Add files via upload

---
+++ code2 day2.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class code2 {
+	public static void main(String[] args) {
+		int a=2;
+		int b=4;
+		int temp=b;
+		b=a;
+		a=temp;
+		System.out.println("a= "+a+ ",b="+b);
+		
+	}
+
+}
